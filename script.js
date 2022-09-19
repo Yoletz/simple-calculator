7dbdbfa54c1525c50d5284f1d8ff9ffbf3b105bc
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-19 15:38:01 +0800

    Refactor script.js

--- script.js
+++ script.js
@@ -5,18 +5,26 @@ for (const child of containerChildren) {
   child.style.gridArea = child.id;
 }
 
+function pressedButton(button) {
+  button.style.transform = "translate(5px, 5px)";
+  button.style.boxShadow = "none";
+}
+
+function unpressedButton(button) {
+  button.style.transform = "translate(0, 0)";
+  button.style.boxShadow = "5px 5px black";
+}
+
 for (const button of buttons) {
   button.addEventListener("mousedown", function (e) {
     if (e.button == 0) {
-      button.style.transform = "translate(5px, 5px)";
-      button.style.boxShadow = "none";
+      pressedButton(button);
     }
   });
 }
 
 document.addEventListener("mouseup", function () {
   for (const button of buttons) {
-  button.style.transform = "translate(0, 0)";
-  button.style.boxShadow = "5px 5px black";
+    unpressedButton(button);
   }
 });
