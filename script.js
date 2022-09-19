f881251a717186dbbe78415e45725041c10a718f
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-19 15:26:09 +0800

    Add style to calculator

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const containerChildren = document.querySelectorAll("#container > *");
+const buttons = document.querySelectorAll("button");
 
 for (const child of containerChildren) {
   child.style.gridArea = child.id;
+}
+
+for (const button of buttons) {
+  button.addEventListener("mousedown", function (e) {
+    if (e.button == 0) {
+      button.style.transform = "translate(5px, 5px)";
+      button.style.boxShadow = "none";
+    }
+  });
+
+  button.addEventListener("mouseup", function () {
+    button.style.transform = "translate(0, 0)";
+    button.style.boxShadow = "5px 5px black";
+  });
 }
