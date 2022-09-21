910f2a3aca00f8e4f7867575d137e42d6e5f5809
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-21 17:59:11 +0800

    Update displayText and add operate function

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
 const containerChildren = document.querySelectorAll("#container > *");
 const buttons = document.querySelectorAll("button");
 const text = document.querySelector("#text");
+const icons = document.querySelectorAll("#oper-icon > *");
 
-let ans = 0;
+let ans = null;
+let currentOperator = "";
+let lastValue = false;
 
 function displayText(btn) {
-  
+
+  if (lastValue) {
+    text.textContent = "";
+    lastValue = false;
+  }
+
+  if (btn.id === "clear") {
+    text.textContent = "";
+    return;
+  }
+
+  if (btn.id === "zero" && text.textContent === "0") {
+    return
+  }
+
   if (text.textContent.length === 15) {
     return;
   }
 
   if (btn.className === "decimal") {
     text.textContent += btn.textContent;
-  } else if (btn.id === "clear") {
-    text.textContent = ""
-    ans = 0;
   } else if (btn.id === "point") {
-
     if (text.textContent.match(/\./)) {
       return;
     }
     text.textContent += btn.textContent;
+  } else if (btn.className === "operate") {
+    operate(btn.id, text.textContent);
+  }
+}
+
+function operate(symbol, txt) {
+  if (!currentOperator) {
+    ans = Number(txt);
+    currentOperator = symbol;
+  } else {
+    switch (currentOperator) {
+      case "add":
+        ans += Number(txt);
+        break;
+      case "sub":
+        ans -= Number(txt);
+        break;
+      case "mul":
+        ans *= Number(txt);
+        break;
+      case "div":
+        ans /= Number(txt);
+        break;
+    }
+  }
+  text.textContent = ans;
+  lastValue = true;
+
+  if (symbol === "equals") {
+    ans = null;
+    currentOperator = "";
   }
 }
 
