824366d9e5bb96b9c9c6dced5a8aa215c863e941
Dondon Ambus <ambus.dondon@gmail.com>
2022-09-21 17:00:11 +0800

    Add decimal class to numeric buttons and prepare displayText() function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const containerChildren = document.querySelectorAll("#container > *");
 const buttons = document.querySelectorAll("button");
+const text = document.querySelector("#text");
+
+function displayText() {
+  
+}
 
 for (const child of containerChildren) {
   child.style.gridArea = child.id;
@@ -8,6 +13,7 @@ for (const child of containerChildren) {
 function pressedButton(button) {
   button.style.transform = "translate(5px, 5px)";
   button.style.boxShadow = "none";
+  displayText(button);
 }
 
 function unpressedButton(button) {
